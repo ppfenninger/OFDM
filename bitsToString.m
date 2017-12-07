64d63c3cf357e24964d3b45cd55ef5bce5badc18
ppfenninger <paige.pfenninger@students.olin.edu>
2017-12-07 11:53:34 -0500

    fixed another thing

--- bitsToString.m
+++ bitsToString.m
@@ -2,17 +2,17 @@ function res = bitsToString(B)
     str = '';
     numNotChar = 0;
     for i = 1:floor(length(B) / 8)
-        char = '';
+        charthing = '';
         for j = 1:8
            if B((i-1)*8 + j) == 1
-               char = strcat(char, '1');
+               charthing = strcat(charthing, '1');
            else
-               char = strcat(char, '0'); 
+               charthing = strcat(charthing, '0'); 
            end
         end
        
-       if ischar(bin2dec(char));
-           str = strcat(str, bin2dec(char)); 
+       if ischar(char(bin2dec(charthing)));
+           str = strcat(str, bin2dec(charthing)); 
        else
            numNotChar = numNotChar + 1; 
        end 
