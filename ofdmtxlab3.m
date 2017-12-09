03e0dea7124440b9f1fb1218053b95298ed547cc
ppfenninger <paige.pfenninger@students.olin.edu>
2017-12-09 11:46:00 -0500

    made ofdmtxlab3

---
+++ ofdmtxlab3.m
@@ -0,0 +1,49 @@
+%% Define constants
+lengthCP = 16;
+numFreqBins = 64;
+lengthWN = 64; % no cyclic prefix
+numWNRepeats = 3; % lenthWN is repeated this many times
+lengthKnownData = 64; % without CP
+numKnownDataRepeats = 4; 
+numDataBins = 10;
+
+%% make the initial bits
+txDataBits = zeros(1, numDataBins*lengthWN); 
+counter = 1; 
+for i = 1:length(txDataBits);
+    if i <= 10*counter
+        txDataBits(i) = 0;
+    elseif i<= 10*counter + 9
+        txDataBits(i) = 1; 
+    else % last one bit, counter goes up
+        txDataBits(i) = 1;
+        counter = counter + 2;
+    end
+end
+
+%% and the known data before hand
+txKnownDataWorkspace = load('knowndatalab.mat');
+txKnownData = txKnownDataWorkspace.known;
+txDataNoCP = [txKnownData, txDataBits];
+
+%% make it a matrix and add the CP
+txDataNoCP = 2*txDataNoCP - 1; % translates from 0 and 1 to -1 and 1
+txParDataNoCP = serialtoParallel(txDataNoCP, numfreqcarriers);
+
+txFreqDataNoCP = ifft(txParDataNoCP.').';
+txFreqData = cyclicprefix(txFreqDataNoCP, lengthCP); 
+
+txDataSerial = reshape(txFreqData.', 1, []); 
+
+%% add white noise in front
+txKnownWNWorkspace = load('wnlab.mat');
+txKnownWN = txKnownWNWorkspace.wn;
+
+txFullWN = [];
+for k = 1:numWNRepeats
+    txFullWN = [txFullWN, txKnownWN];
+end
+
+txFullData = [txFullWN, txDataSerial]; 
+
+write_usrp_data_file(txFullData, 'txlab.dat'); 
